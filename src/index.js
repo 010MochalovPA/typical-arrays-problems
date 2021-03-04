099812f18473bd923f27274df3a9b2be7672b027
010MochalovPA <78229095+010MochalovPA@users.noreply.github.com>
2021-03-04 21:55:24 +0300

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0;
+  }
+  let minResult = array[0];
+  array.forEach(item => { if (minResult > item) minResult = item });
+  return minResult;    
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0;
+  }
+  let maxResult = array[0];
+  array.forEach(item => { if (maxResult < item) maxResult = item });
+  return maxResult;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0;
+  }
+  return array.reduce((sum, current) => sum + current, 0) / array.length;
 }
